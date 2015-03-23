486d18f5b9cf85de642eecf318e7135939e7a5ac
Coralie <ruiz.coralie64@live.fr>
2015-03-23 22:02:31 +0100

    Enter date to find archive event

--- TpRails/app/controllers/dashboards_controller.rb
+++ TpRails/app/controllers/dashboards_controller.rb
@@ -9,6 +9,19 @@ class DashboardsController < ApplicationController
 
 	def show
 		@dashboard = Dashboard.find(params[:id])
+
+		archiveDate = @dashboard.dateArchive.strftime("%Y-%m-%d")
+  		archivePath = "http://data.githubarchive.org/"+ archiveDate +"-12.json.gz"
+
+  		gz = open(archivePath)
+		js = Zlib::GzipReader.new(gz).read
+ 		
+ 		#Recalcul des données vu que bdd limitée
+ 		#Afficher les données dans un tableau à la place du print : Créer un tableau dans la vue SHOW.html.erb
+		#Yajl::Parser.parse(js) do |event|
+			#print event
+		#end 
+
 	end
 
 	#Get
@@ -18,17 +31,19 @@ class DashboardsController < ApplicationController
   	#Post
   	def create
   		@dashboard =Dashboard.new(params.require(:dashboard).permit(:title, :dateArchive))
-  		print @dashboard
-  		print @dashboard.dateArchive
-  		@dashboard.save
 
-		gz = open('http://data.githubarchive.org/2015-01-01-12.json.gz')
-		js = Zlib::GzipReader.new(gz).read
- 
-		#Yajl::Parser.parse(js) do |event|
-			#print event
-		#end 
-		redirect_to @dashboard
-  
+  		archiveDate = @dashboard.dateArchive.strftime("%Y-%m-%d")
+  		if(Date.new(2012,10,12) < @dashboard.dateArchive &&  @dashboard.dateArchive < Date.today)
+  			@dashboard.save
+  		end	
+  		
+		redirect_to @dashboard 
    	end
+
+   	def destroy
+    	@dashboard = Dashboard.find(params[:id])
+    	@dashboard.destroy
+ 
+    	redirect_to dashboards_path
+  end
 end
