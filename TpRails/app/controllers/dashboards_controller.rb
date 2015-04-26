2d79122dfc0363ec3316a61a9f06424103742311
Coralie <ruiz.coralie64@live.fr>
2015-04-26 14:48:11 +0200

    Display chart incomplete

--- TpRails/app/controllers/dashboards_controller.rb
+++ TpRails/app/controllers/dashboards_controller.rb
@@ -9,11 +9,22 @@ class DashboardsController < ApplicationController
 
 	def show
 		@dashboard = Dashboard.find(params[:id])
-  
+    @archives = Archive.all
+    
     # Recupération des archives correspondantes au dashboard + Affichage en graph
 
 		#datetime = DateTime.strptime(@dashboard.dateArchive, '%m/%d/%Y %H:%M')
     #datetime.hour # => 21
+
+    #db = MongoClient.new("ds031641.mongolab.com", 31641).db("tprails")
+    #mongodb://tprails:ingesup33@ds031641.mongolab.com:31641/tprails
+
+    #resultByType = db.archives.aggregate(
+      #[
+        #{ $group: { _id: "type", count: { $sum: 1 } } } 
+      #]
+    #)
+
 
 	end
 
@@ -25,7 +36,7 @@ class DashboardsController < ApplicationController
   	def create
   		@dashboard =Dashboard.new(params.require(:dashboard).permit(:title, :dateArchive))
       archiveHour = Integer(@dashboard.dateArchive)
-  		#archiveDate = @dashboard.dateArchive.strftime("%Y-%m-%d")
+      
   		if(00 < archiveHour &&  archiveHour < 23)
   			@dashboard.save      
   		end	
